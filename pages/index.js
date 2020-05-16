92f686abd6bde463c1244c1622bc3238791bb0a9
Jonatan Bergquist <jonatan.h.bergquist@gmail.com>
2020-05-16 14:23:18 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
+
 
 const Home = () => (
   <div>
     <h1>Next.js on the [JAMstack](https://jamstack.org)</h1>
 
-    <h3>Hooray 🎉 - you've built this with <a href="https://nextjs.org">Next.js</a>!</h3>
+    Read <Link href="/posts/first-post"><a>this page!</a></Link>
 
     <style jsx>{`
       :global(html,body) {
